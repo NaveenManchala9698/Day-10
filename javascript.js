b6649e2b4d401871f7c4c94c8dd10a51d38608c0
Naveen Kumar Manchala <naveenmanchala9698@gmail.com>
2022-05-06 13:25:28 +0200

    JS Basics

---
+++ javascript.js
@@ -0,0 +1,70 @@
+// JS Basics
+// Exercise A
+
+let test = "Naveen"
+
+//Exercise B
+
+let number = 20
+let sum=0
+
+for (let i = 10; i <= number; i++) {
+    sum += i;
+}
+console.log('The sum of numbers is:', sum )
+
+// Exercise C
+
+console.log(Math.floor(Math.random( ) * 20))
+
+// Exercise D
+
+let me = {
+    name : "Naveen",
+    surname : "Manchala",
+    age : 23
+}
+
+// Exercise E
+
+delete me.age
+console.log(me)
+
+//Exercise F
+
+me.skills = ['HTML', 'CSS', 'JavaScript']
+console.log(me)
+
+// Exercise G
+
+me.skills.pop()
+console.log(me)
+
+
+// JS Functions
+
+// Exercise 1
+
+function dice(max, min) {
+    console.log((Math.floor(Math.random() * (max-min + 1) + min)))
+}
+
+dice(1, 6)
+
+// Exercise 2
+
+function whoIsBigger(a, b) {
+    if (a > b) {
+        console.log(a)
+    } else {
+        console.log(b)
+    }
+}
+whoIsBigger(6, 4)
+
+// Exercise 3
+
+function splitMe() {
+    console.log(splitMe.split(''))
+}
+splitMe('I am Happy')
